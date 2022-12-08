f48ebd25b5590f0074ba9b5f9ad9c64a5fb491b6
Larissa Yasmim <yasmimlarissa09@gmail.com>
2022-12-08 15:54:00 -0300

    Adiciona tooltip e modal

---
+++ js/script.js
@@ -0,0 +1,25 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/acordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initdropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+import initFuncionamento from './modules/funcionamento.js'
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initdropDownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
+
+
+

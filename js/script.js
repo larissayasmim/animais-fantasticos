b7a1264aeb872d160a7776a0bfe3d08c07535f1f
Larissa Yasmim <yasmimlarissa09@gmail.com>
2022-12-13 23:51:03 -0300

    Modifica alguns estilos.

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@ import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initdropDownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js'
-import initAnimaNumeros from './modules/anima-numeros.js'
-import initFuncionamento from './modules/funcionamento.js'
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin';
+
 
 
 initScrollSuave();
@@ -18,8 +20,7 @@ initModal();
 initTooltip();
 initdropDownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
-
-
+initFetchAnimais();
+initFetchBitcoin();
 
